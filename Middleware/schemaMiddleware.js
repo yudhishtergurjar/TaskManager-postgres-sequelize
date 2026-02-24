a196309eb88a13922d6c01b41f48d54c6c6d482d
yudhishtergurjar <yudhishtergurjar001@gmail.com>
2026-02-24 10:29:55 +0530

    changed authMiddleware

--- Middleware/schemaMiddleware.js
+++ Middleware/schemaMiddleware.js
@@ -1,6 +1,8 @@
 // import { userRegisterSchema, userLoginSchema } from "../Schema/schema";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
+import db from "../models/index.js";
+const { User } = db;
 
 const jwtSecret = process.env.jwtSecret;
 
@@ -13,7 +15,7 @@ export function  schemaMiddleware(schema){
         next();
     }
 }
-export const authMiddleware = (req,res,next)=>{
+export const authMiddleware = async (req,res,next)=>{
     const authHeader = req.headers.authorization;
     if (!authHeader) { 
         return res.status(401).json({ message: "Token missing" });
@@ -22,6 +24,11 @@ export const authMiddleware = (req,res,next)=>{
         const token = authHeader.split(' ')[1];
         const decode = jwt.verify(token,jwtSecret);
         req.user=decode;
+        const user = await User.findOne({
+            where:{email:decode.email}
+        });
+        if(!user) return res.status(400).json({message:"user not found"});
+        if(!user.refreshToken) return res.status(400).json({message:"session logout"});
         next();
     }catch{
         res.status(400).json("invalid token");
